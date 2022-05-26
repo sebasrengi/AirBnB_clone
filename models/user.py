198ac4cf74796675089d08915533edbe596919e5
sebasrengi <85517658+sebasrengi@users.noreply.github.com>
2022-05-25 23:59:49 -0500

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-"""User Class"""
-from models.base_model import BaseModel
+"""This module defines a class User"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import String, Column
+from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
-    """User Class inherit from BaseModel"""
+class User(BaseModel, Base):
+    """This class defines a user by various attributes"""
+    __tablename__ = 'users'
 
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship("Place", cascade="all, delete", backref="user")
+    review = relationship("Review", cascade="all, delete", backref="user")
