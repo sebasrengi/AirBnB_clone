12726b9e4e180df190ded724c9bef7041fa64bb9
sebasrengi <85517658+sebasrengi@users.noreply.github.com>
2022-05-25 23:59:06 -0500

    Update place.py

--- models/place.py
+++ models/place.py
@@ -1,19 +1,73 @@
 #!/usr/bin/python3
-"""User Class"""
-from models.base_model import BaseModel
+""" Place Module for HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, ForeignKey, Integer, String, Float, Table
+from sqlalchemy.orm import relationship
+from models.review import Review
+
+import models
+import os
+
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60),
+                             ForeignKey('places.id'),
+                             primary_key=True, nullable=False),
+                      Column('amenity_id', String(60),
+                             ForeignKey('amenities.id'),
+                             primary_key=True, nullable=False))
 
 
-class Place(BaseModel):
-    """Place Class inherit from BaseModel"""
+class Place(BaseModel, Base):
+    """ A place to stay """
 
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+    __tablename__ = 'places'
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(Integer, default=0, nullable=False)
+    max_guest = Column(Integer, default=0, nullable=False)
+    price_by_night = Column(Integer, default=0, nullable=False)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
     amenity_ids = []
+
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+
+        amenities = relationship("Amenity", secondary='place_amenity',
+                                 viewonly=False, cascade="all, delete",
+                                 backref="places")
+
+        reviews = relationship("Review",
+                               cascade="all, delete",
+                               backref="place")
+
+    else:
+        @property
+        def reviews(self):
+            """returns the list of City instances with state_id"""
+            reviews_list = []
+            _reviews = models.storage.all(Review)
+            for _review in _reviews.values():
+                if self.id == _review.place_id:
+                    reviews_list.append(_review)
+            return reviews_list
+
+        @property
+        def amenities(self):
+            """returns the list of City instances with state_id"""
+            from models.amenity import Amenity
+            amenities_list = []
+            _amenities = models.storage.all(Amenity)
+            for _amenity in _amenities.values():
+                if _amenity.id in self.amenity_ids:
+                    amenities_list.append(_amenity)
+            return amenities_list
+
+        @amenities.setter
+        def amenities(self, arg):
+            from models.amenity import Amenity
+            if arg.__class__ == Amenity:
+                self.amenity_ids = arg
+                
