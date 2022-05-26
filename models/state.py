6932f48702fcbaeca5340620173ba01d9d3d8488
sebasrengi <85517658+sebasrengi@users.noreply.github.com>
2022-05-25 23:57:50 -0500

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,8 +1,31 @@
 #!/usr/bin/python3
-"""User Class"""
-from models.base_model import BaseModel
+""" State Module for HBNB project """
+import models
+from models.city import City
+from models.base_model import BaseModel, Base
+from sqlalchemy import String, Column
+from sqlalchemy.orm import relationship
+import os
 
 
-class State(BaseModel):
-    """class State inherited from BaseModel"""
-    name = ""
+class State(BaseModel, Base):
+    """ State class """
+    __tablename__ = 'states'
+
+    name = Column(String(128), nullable=False)
+
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+
+        cities = relationship("City", cascade="all, delete", backref="states")
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            """returns the list of City instances with state_id"""
+            cities_list = []
+            _cities = models.storage.all(City)
+            for _city in _cities.values():
+                if self.id == _city.state_id:
+                    cities_list.append(_city)
+            return cities_list
