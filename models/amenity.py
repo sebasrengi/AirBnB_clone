969da7fbcdd149d93271e40f43e49bda34905903
sebasrengi <85517658+sebasrengi@users.noreply.github.com>
2022-05-25 23:58:32 -0500

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
-"""User Class"""
-from models.base_model import BaseModel
+""" State Module for HBNB project """
+from os import getenv
+from models.base_model import BaseModel, Base
+from sqlalchemy import String, Column
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    """Amenity class inherit from BaseModel"""
-    name = ""
+class Amenity(BaseModel, Base):
+    """ Class Amenity """
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        place_amenities = relationship("Place", secondary='place_amenity',
+                                       back_populates="amenities")
+        
