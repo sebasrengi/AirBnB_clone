f9354523510c226a7e111120ed121a2a0fbec2ba
sebasrengi <85517658+sebasrengi@users.noreply.github.com>
2022-05-25 23:58:48 -0500

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
-"""User Class"""
-from models.base_model import BaseModel
+""" City Module for HBNB project """
+
+from models.base_model import BaseModel, Base
+from sqlalchemy import String, Column, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
-    """City Class inherit from BaseModel"""
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """ The city class, contains state ID and name """
+    __tablename__ = 'cities'
+
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    places = relationship("Place", cascade="all, delete", backref="cities")
